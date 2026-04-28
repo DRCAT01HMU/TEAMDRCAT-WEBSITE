b50feb13cf102201da745d3ef4288ecf44f1daf5
DRCAT01HMU <drcat99hmu@gmail.com>
2026-04-28 23:23:23 +0700

    Add files via upload

---
+++ graph_data.js
@@ -0,0 +1,79 @@
+const graphData = {
+  "nodes": [
+  {
+    "id": "TEAMDRCAT",
+    "name": "TEAMDRCAT WEBSITE",
+    "group": 0,
+    "size": 80,
+    "url": ""
+  },
+  {
+    "id": "Nội Khoa",
+    "name": "Nội Khoa",
+    "group": 1,
+    "size": 40,
+    "url": "https://drcat01hmu.github.io/TEAMDRCAT-APP-NOI-KHOA/"
+  },
+  {
+    "id": "Ngoại Khoa",
+    "name": "Ngoại Khoa",
+    "group": 1,
+    "size": 40,
+    "url": "https://drcat01hmu.github.io/TEAMDRCAT-APP-NGOAI-KHOA/"
+  },
+  {
+    "id": "Sản Khoa",
+    "name": "Sản Khoa",
+    "group": 1,
+    "size": 40,
+    "url": "https://drcat01hmu.github.io/TEAMDRCAT-APP-SAN-KHOA/"
+  },
+  {
+    "id": "Nhi Khoa",
+    "name": "Nhi Khoa",
+    "group": 1,
+    "size": 40,
+    "url": "https://drcat01hmu.github.io/TEAMDRCAT-APP-NHI-KHOA/"
+  },
+  {
+    "id": "Giải Phẫu",
+    "name": "Giải Phẫu",
+    "group": 1,
+    "size": 40,
+    "url": "https://drcat01hmu.github.io/TEAMDRCAT-APP-GIAI-PHAU/"
+  },
+  {
+    "id": "Sinh Lý",
+    "name": "Sinh Lý",
+    "group": 1,
+    "size": 40,
+    "url": "https://drcat01hmu.github.io/TEAMDRCAT-APP-SINH-LY/"
+  }
+],
+  "links": [
+  {
+    "source": "TEAMDRCAT",
+    "target": "Nội Khoa"
+  },
+  {
+    "source": "TEAMDRCAT",
+    "target": "Ngoại Khoa"
+  },
+  {
+    "source": "TEAMDRCAT",
+    "target": "Sản Khoa"
+  },
+  {
+    "source": "TEAMDRCAT",
+    "target": "Nhi Khoa"
+  },
+  {
+    "source": "TEAMDRCAT",
+    "target": "Giải Phẫu"
+  },
+  {
+    "source": "TEAMDRCAT",
+    "target": "Sinh Lý"
+  }
+]
+};
